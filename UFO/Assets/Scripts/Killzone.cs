21bbc1c66a3687efe868af9f7c4d5f6631da3da1
JPierson1313 <jpierso3@masonlive.gmu.edu>
2020-05-24 22:52:25 -0400

    Update Killzone.cs

--- UFO/Assets/Scripts/Killzone.cs
+++ UFO/Assets/Scripts/Killzone.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script is used for an empty gameobject that has a box collider on it that will destroy the pillars when they reach the end of the left side of the board
+/// </summary>
 public class Killzone : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        //Basic code of destroying any gameobject that collides with the object this script is attach to
         if(collision.gameObject)
         {
             Destroy(collision.gameObject);
