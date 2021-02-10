df8cba828cd5052f4175fc44c65e4bb3a44df9fb
JPierson1313 <jpierso3@masonlive.gmu.edu>
2021-02-10 00:39:15 -0600

    Update ThrusterSystem.cs

--- UFO/Assets/Scripts/ThrusterSystem.cs
+++ UFO/Assets/Scripts/ThrusterSystem.cs
@@ -13,6 +13,7 @@ public class ThrusterSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This code is mainly use to turn the Thrusters game object off when the player is not tapping/clicking the screen to make the UFO move upwards
         if(gameObject.activeInHierarchy)
         {
             gameObject.SetActive(false);
