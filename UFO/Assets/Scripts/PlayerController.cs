5e7ef759dfe7766a5c58246d71e190e85f2c22a3
JPierson1313 <jpierso3@masonlive.gmu.edu>
2021-02-10 00:37:12 -0600

    Update PlayerController.cs

--- UFO/Assets/Scripts/PlayerController.cs
+++ UFO/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Player Systems")]
-    public float jumpForce = 100;
-    public GameObject player;
-    public GameObject Thrusters;
-    public bool isDead;
-    public ScoreSystem ss;
-
-    // Start is called before the first frame update
+    public float jumpForce = 100; //Force use to move the player up
+    public GameObject player; //Player game object
+    public GameObject Thrusters; //Thruster game object
+    public bool isDead; //Boolean use to check if the player is dead or not
+    public ScoreSystem ss; //Script used for keeping track of score
+    
     void Awake()
     {
-        player = gameObject;
+        //When the script is awake, we find the game object that contains the ScoreSystem script and set the score to 0
         ss = GameObject.FindGameObjectWithTag("SS").GetComponent<ScoreSystem>();
         ss.score = 0;
     }
@@ -22,7 +21,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //When isDead boolean is false, the player can tap/click on the screen to set the player's velocity to zero, 
+        //add force in the y axis based on the jumpForce variable and have thrusters art be set active to true
         if (!isDead)
         {
             if (Input.GetMouseButtonDown(0))
@@ -34,20 +34,21 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
+        //When the player collides with any colliders, like the floor, ceiling, or pillars, then the isDead boolean will be set to true
         if(collision.gameObject)
         {
             isDead = true;
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
+        //When the player collides with the trigger box that is between the pillar, a point will be added to the score
         if(other.gameObject)
         {
             ss.score += 1;
         }
     }
-
 }
