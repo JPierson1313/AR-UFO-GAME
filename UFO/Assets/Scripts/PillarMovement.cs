d3a2aec5b84341fc3bed03531e47365f6353a5f5
JPierson1313 <jpierso3@masonlive.gmu.edu>
2021-02-10 00:06:27 -0600

    Update PillarMovement.cs

--- UFO/Assets/Scripts/PillarMovement.cs
+++ UFO/Assets/Scripts/PillarMovement.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class PillarMovement : MonoBehaviour
 {
-    public PlayerController pc;
+    PlayerController pc; //PlayController script
 
     // Start is called before the first frame update
     void Start()
     {
+        //Finds the player game object that has the PlayerController script attach to it and set it to the variable name pc
         pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If isDead boolean set to true, then we tell the pillar to stop moving by setting its velocity to 0;
         if (pc.isDead)
         {
             GetComponent<Rigidbody>().velocity = Vector2.zero;
