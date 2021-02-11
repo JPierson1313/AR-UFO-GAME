8978693380acd90a79a8d7d02f0d6bb17bbb2f7a
JPierson1313 <jpierso3@masonlive.gmu.edu>
2021-02-10 21:40:36 -0600

    Update ScoreSystem.cs

--- UFO/Assets/Scripts/ScoreSystem.cs
+++ UFO/Assets/Scripts/ScoreSystem.cs
@@ -5,16 +5,17 @@ using TMPro;
 
 public class ScoreSystem : MonoBehaviour
 {
-    public int score = 0;
-    public int highScore = 0;
-    string highScoreKey = "HighScore";
+    public int score = 0; //Score
+    public int highScore = 0; //High Score
+    string highScoreKey = "HighScore"; //string key use to help save the high score
 
-    public TextMeshPro scoreText;
-    public TextMeshPro highScoreText;
+    public TextMeshPro scoreText; //Text use to display Score
+    public TextMeshPro highScoreText; //Text use to display the High Score
 
     // Start is called before the first frame update
     void Start()
     {
+        //At the start, we have the high score set to the current high score while also setting score to 0
         highScore = PlayerPrefs.GetInt(highScoreKey, 0);
         score = 0;
     }
@@ -22,8 +23,12 @@ public class ScoreSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Set the text for both scoreText and highScoreText to their respective scores
         scoreText.text = score.ToString();
         highScoreText.text = "Best:\n" + highScore.ToString();
+        
+        //If the score is greater than our current high score, we set the high score equal to the score
+        //Set the player preference to our highScoreKey and current score and then save it to PlayerPrefs
         if (score > highScore)
         {
             highScore = score;
